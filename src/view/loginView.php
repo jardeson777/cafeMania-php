63f820cdae247ae4344da6d6d001757fdcd0ca36
jnogueira <jnogueira@marlin.com.br>
2022-06-16 13:12:08 -0300

    chore: ajustando estrutura de pasta

--- src/view/loginView.php
+++ src/view/loginView.php
@@ -9,7 +9,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Login</title>
-        <?php include '../utils/bootstrap.php'; ?>
+        <?php include '../includes/links.php'; ?>
     </head>
 
     <body>
