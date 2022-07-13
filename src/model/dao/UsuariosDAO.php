e3013e9368aacd3bd0f7f4a272e2d20deff25332
denerson28 <denerson2808@hotmail.com>
2022-07-12 23:00:38 -0300

    iniciando a documentação

--- src/model/dao/UsuariosDAO.php
+++ src/model/dao/UsuariosDAO.php
@@ -13,6 +13,13 @@ class UsuariosDao
 {
   private $connection;
 
+    /*
+    No construtor da classe, é a parte em que o DAO abre a contexão com o banco de dados
+    Instanciando um objeto do tipo ConexaoBanco, na qual possui o método getConnection
+    Logo em seguida, ele atribui o valor da operação feita com o objeto na variável
+    connection, que passa a ser utilizada nos métodos do UsuariosDao, evitando assim
+    a repetição de código de sempre ter que abrir a conexão em cada método.
+  */
   function __construct()
   {
     $conexaoBanco = new ConexaoBanco();
