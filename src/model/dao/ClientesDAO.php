9e9468a2e6544ffd2e15ad9b633f308ed66b57de
jardeson777 <jardeson777@gmail.com>
2022-07-13 19:55:33 -0300

    feat: inicio efetuar venda gerente

--- src/model/dao/ClientesDAO.php
+++ src/model/dao/ClientesDAO.php
@@ -1,3 +1,18 @@
 <?php
-    
-?>
+
+namespace dao;
+
+require 'connection.php';
+
+use Conexao\ConexaoBanco;
+
+class ClienteDAO
+{
+  private $connection;
+
+  function __construct()
+  {
+    $conexaoBanco = new ConexaoBanco();
+    $this->connection = $conexaoBanco->getConnection();
+  }
+}
