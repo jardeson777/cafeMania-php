63bce5cbc32a3a226d973141d9cf93dddb4f2df5
denerson28 <denerson2808@hotmail.com>
2022-07-13 23:06:14 -0300

    Adicionando Documentação nas interfaces

--- src/model/interface/builder.php
+++ src/model/interface/builder.php
@@ -2,8 +2,19 @@
 
 namespace interface;
 
+/*
+Interface é responsável por instanciar as classes dos relatórios da aplicação
+que possuem métodos e atributos semelhantes.
+*/
 interface Builder
 {
+  /*
+    função que busca os dados para geração do relatório
+  */
   public function BuscarDados(string $query);
+
+  /*
+  função que constrói o relatório com os dados que vão ser acessados internamente
+  */
   public function ConstroiRelatorio();
 }
--- src/model/interface/pagamento.php
+++ src/model/interface/pagamento.php
@@ -2,7 +2,18 @@
 
 namespace interface;
 
+
+/*
+Interface Responsável na parte de pagamentos, que possui a função RealizarVenda
+que é usada de diferentes formas de acordo com a forma de pagamento escolhida.
+*/
 interface Pagamento
 {
-  private float $totalVenda;
+
+  /*
+  Função que realiza a venda e faz todas as operações necessárias
+  de acordo com cada forma de pagamento que vai ser utilizada na venda
+  de algum produto escolhido
+  */
+  public function RealizarVenda();
 }
